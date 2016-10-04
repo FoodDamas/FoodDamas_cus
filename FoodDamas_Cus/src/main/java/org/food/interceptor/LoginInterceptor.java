73a87cb75178c9ce040633891c8f001b4d4e7f74
Leeek <Leeek@LeeeK>
2016-10-04 09:35:47 +0900

    1004_09:35_이가영 이상하다..

--- FoodDamas_Cus/src/main/java/org/food/interceptor/LoginInterceptor.java
+++ FoodDamas_Cus/src/main/java/org/food/interceptor/LoginInterceptor.java
@@ -43,7 +43,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
       // response.sendRedirect("/");
       Object dest = session.getAttribute("dest");
      
-      response.sendRedirect(dest != null ? (String) dest : "/");
+      response.sendRedirect(dest != null ? (String) dest : "/home");
     }else{
         response.sendRedirect("/member/login?result=fail");
 
